2e6f0674511d68221a38e10613ae133d6333e2cc
Ajay Pannala <ajaypannala01@gmail.com>
2026-03-04 12:26:45 +0530

    upadte Repository

--- src/main/java/com/ajay/Bus/Scheduling/and/RouteManagement/System/repository/UserRepository.java
+++ src/main/java/com/ajay/Bus/Scheduling/and/RouteManagement/System/repository/UserRepository.java
@@ -1,5 +1,12 @@
 package com.ajay.Bus.Scheduling.and.RouteManagement.System.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ajay.Bus.Scheduling.and.RouteManagement.System.entity.User;
+
+public interface UserRepository extends JpaRepository<User,Integer>{
+	public User findByEmail(String email);
+	public User findByEmailAndPassword(String email,String password);
+	public User findByRole(String role);
 
 }
